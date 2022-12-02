e21688ff49cb7e6123bb8ece59d55947f44e0e0d
Harry Dobbs <hrdobbs@outlook.com>
2022-12-02 06:21:09 +0000

    adds update page details

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useParams} from 'react-router-dom';
 
-const UpdateRestaurant = () => {
+const UpdateRestaurant = (props) => {
+  const {id} = useParams();
+  const [name, setName] =useState();
+  const [location, setLocation] =useState();
+  const [priceRange, setPriceRange] =useState();
   return (
-    <div>UpdateRestaurant</div>
+    <div>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input id="name" className="form-control" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="location">Location</label>
+          <input id="location" className="form-control" type="text" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price_range">Price Range</label>
+          <input id="price_range" className="form-control" type="number" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
 
